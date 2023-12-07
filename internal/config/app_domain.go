2ae86e65e018135b54b933341afab74b8137f698
mark wu <saybyebyet@gmail.com>
2023-12-07 13:20:31 +0800

    Add optional flag for domain verification

--- internal/config/app_domain.go
+++ internal/config/app_domain.go
@@ -1,6 +1,7 @@
 package config
 
 type AppDomainConfig struct {
-	Domain string `json:"domain" pageship:"required,max=200,hostname_port|hostname_rfc1123,lowercase"`
-	Site   string `json:"site" pageship:"required,dnsLabel"`
+	Domain             string `json:"domain" pageship:"required,max=200,hostname_port|hostname_rfc1123,lowercase"`
+	Site               string `json:"site" pageship:"required,dnsLabel"`
+	DomainVerification bool   `json:"domainVerification" pageship:"omitempty"`
 }
